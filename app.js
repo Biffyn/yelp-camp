167f6976e694ec20aa1a2992a21d9942532289b7
James Richards <nuravenweb@gmail.com>
2017-02-02 21:17:16 +0000

    Added models and moved mongoose schema for campgrounds.

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
 var express     = require("express"),
     app         = express(),
     bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose");
+    mongoose    = require("mongoose"),
+    Campground  = require("./models/campground");
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 mongoose.connect("mongodb://localhost/yelp_camp");
-
-// ***Schema Setup***
-
-var campgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    description: String
-});
-
-var Campground = mongoose.model("Campground", campgroundSchema);
 
 // ***ROUTES***
 

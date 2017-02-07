7edce8038620de1e618ac761c18087c673bcfbf0
James Richards <nuravenweb@gmail.com>
2017-02-07 20:40:14 +0000

    Added authorisation ann middleware

--- middleware/index.js
+++ middleware/index.js
@@ -21,7 +21,7 @@ middlewareObj.checkCampgroundOwnership = function(req, res, next) {
     } else {
         res.redirect("back");
     }
-};
+}
 
 middlewareObj.checkCommentOwnership = function(req, res, next) {
  if(req.isAuthenticated()){
@@ -40,13 +40,13 @@ middlewareObj.checkCommentOwnership = function(req, res, next) {
     } else {
         res.redirect("back");
     }
-};
+}
 
 middlewareObj.isLoggedIn = function(req, res, next){
     if(req.isAuthenticated()){
         return next();
     }
     res.redirect("/login");
-};
+}
 
 module.exports = middlewareObj;
